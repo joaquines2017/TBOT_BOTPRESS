221ed4b1ccc855ad59faa69266dbcb1dea367523
joaquin-casa <joaquin.nes@gmail.com>
2025-07-22 17:19:26 -0300

    Update: Configuración del servidor - db.js y Dockerfile actualizado

--- backend/server/db.js
+++ backend/server/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg')
 
 const pool = new Pool({
   user: 'postgres',
-  host: 'host.docker.internal',
+  host: 'localhost',
   database: 'web-tbot',
   password: 'indio14!',
   port: 5432,
